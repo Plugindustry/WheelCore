5960b835fa2240b769a4dd6b29e6a238f53cc8c0
timetraveler314 <18847669238@163.com>
2019-08-12 03:39:00 +0800

    Added isWire in BlockUtil

--- src/main/java/com/IndustrialWorld/utils/BlockUtil.java
+++ src/main/java/com/IndustrialWorld/utils/BlockUtil.java
@@ -10,5 +10,5 @@ public class BlockUtil {
 
     public boolean isIWBlock(Block block) { return MainManager.hasBlock(block); }
     public boolean isMachine(Block block) { return isIWBlock(block) ? MainManager.getInstanceFromId(MainManager.getBlockId(block)) instanceof MachineBlock : false; }
-    public boolean isWire(Block block) { return isIWBlock(block) ? MainManager.getInstanceFromId(MainManager.getBlockId(block)) instanceof Wire : false }
+    public boolean isWire(Block block) { return isIWBlock(block) ? MainManager.getInstanceFromId(MainManager.getBlockId(block)) instanceof Wire : false; }
 }
