84590ea49ec83aa207ce97fe319c7fe53dcb3510
czm23333 <2581528358@qq.com>
2019-08-12 12:24:35 +0800

    Improve BlockUtil

--- src/main/java/com/IndustrialWorld/utils/BlockUtil.java
+++ src/main/java/com/IndustrialWorld/utils/BlockUtil.java
@@ -3,12 +3,9 @@ package com.IndustrialWorld.utils;
 import com.IndustrialWorld.interfaces.MachineBlock;
 import com.IndustrialWorld.interfaces.Wire;
 import com.IndustrialWorld.manager.MainManager;
-import org.bukkit.Location;
 import org.bukkit.block.Block;
 
 public class BlockUtil {
-
-    public boolean isIWBlock(Block block) { return MainManager.hasBlock(block); }
-    public boolean isMachine(Block block) { return isIWBlock(block) ? MainManager.getInstanceFromId(MainManager.getBlockId(block)) instanceof MachineBlock : false; }
-    public boolean isWire(Block block) { return isIWBlock(block) ? MainManager.getInstanceFromId(MainManager.getBlockId(block)) instanceof Wire : false; }
+    public static boolean isMachine(Block block) { return MainManager.hasBlock(block) ? MainManager.getInstanceFromId(MainManager.getBlockId(block)) instanceof MachineBlock : false; }
+    public static boolean isWire(Block block) { return MainManager.hasBlock(block) ? MainManager.getInstanceFromId(MainManager.getBlockId(block)) instanceof Wire : false; }
 }
