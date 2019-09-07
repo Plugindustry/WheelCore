caabdb34184bf99ab0ae845adb1d7fc73edd97e5
Lucky_fish <Lucky_fishOwO@qq.com>
2019-09-07 23:03:03 +0800

    Remove onInteractAsItem because it isn't required any longer.

--- src/main/java/com/IndustrialWorld/interfaces/ItemBase.java
+++ src/main/java/com/IndustrialWorld/interfaces/ItemBase.java
@@ -1,7 +1,5 @@
 package com.IndustrialWorld.interfaces;
 
-import org.bukkit.event.player.PlayerInteractEvent;
-
 public abstract class ItemBase extends Interactive {
-    public void onInteractAsItem(PlayerInteractEvent event){}
+
 }
