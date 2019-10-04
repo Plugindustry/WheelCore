f276776f3933c193a05a97e28463395c100e2582
timetraveler314 <36299842+timetraveler314@users.noreply.github.com>
2019-10-04 11:23:06 +0800

    changed "-" to "_" in keys of language files

--- src/main/java/com/IndustrialWorld/i18n/I18nConst.java
+++ src/main/java/com/IndustrialWorld/i18n/I18nConst.java
@@ -2,42 +2,42 @@ package com.IndustrialWorld.i18n;
 
 public class I18nConst {
     public static class Item {
-        public static String FORGE_HAMMER = "item.forge-hammer.name";
-        public static String FORGE_HAMMER_LORE1 = "item.forge-hammer.lore.1";
-        public static String FORGE_HAMMER_LORE2 = "item.forge-hammer.lore.2";
-        public static String FORGE_HAMMER_LORE3 = "item.forge-hammer.lore.3";
+        public static String FORGE_HAMMER = "item.forge_hammer.name";
+        public static String FORGE_HAMMER_LORE1 = "item.forge_hammer.lore.1";
+        public static String FORGE_HAMMER_LORE2 = "item.forge_hammer.lore.2";
+        public static String FORGE_HAMMER_LORE3 = "item.forge_hammer.lore.3";
 
-        public static String IW_CRAFTING_TABLE = "item.iw-crafting-table.name";
-        public static String IW_CRAFTING_TABLE_LORE1 = "item.iw-crafting-table.lore.1";
-        public static String IW_CRAFTING_TABLE_LORE2 = "item.iw-crafting-table.lore.2";
+        public static String IW_CRAFTING_TABLE = "item.iw_crafting_table.name";
+        public static String IW_CRAFTING_TABLE_LORE1 = "item.iw_crafting_table.lore.1";
+        public static String IW_CRAFTING_TABLE_LORE2 = "item.iw_crafting_table.lore.2";
 
-        public static String IRON_PLATE = "item.iron-plate.name";
-        public static String IRON_PLATE_LORE1 = "item.iron-plate.lore.1";
-        public static String IRON_PLATE_LORE2 = "item.iron-plate.lore.2";
+        public static String IRON_PLATE = "item.iron_plate.name";
+        public static String IRON_PLATE_LORE1 = "item.iron_plate.lore.1";
+        public static String IRON_PLATE_LORE2 = "item.iron_plate.lore.2";
 
         public static String CUTTER = "item.cutter.name";
         public static String CUTTER_LORE1 = "item.cutter.lore.1";
         public static String CUTTER_LORE2 = "item.cutter.lore.2";
 
-        public static String BASIC_MACHINE_BLOCK = "item.basic-machine-block.name";
-        public static String BASIC_MACHINE_BLOCK_LORE1 = "item.basic-machine-block.lore.1";
-        public static String BASIC_MACHINE_BLOCK_LORE2 = "item.basic-machine-block.lore.2";
+        public static String BASIC_MACHINE_BLOCK = "item.basic_machine_block.name";
+        public static String BASIC_MACHINE_BLOCK_LORE1 = "item.basic_machine_block.lore.1";
+        public static String BASIC_MACHINE_BLOCK_LORE2 = "item.basic_machine_block.lore.2";
 
-        public static String COPPER_INGOT = "item.basic-machine-block.name";
-        public static String COPPER_INGOT_LORE1 = "item.basic-machine-block.lore.1";
-        public static String COPPER_INGOT_LORE2 = "item.basic-machine-block.lore.2";
+        public static String COPPER_INGOT = "item.basic_machine_block.name";
+        public static String COPPER_INGOT_LORE1 = "item.basic_machine_block.lore.1";
+        public static String COPPER_INGOT_LORE2 = "item.basic_machine_block.lore.2";
 
-        public static String COPPER_PLATE = "item.copper-plate.name";
-        public static String COPPER_PLATE_LORE1 = "item.copper-plate.lore.1";
-        public static String COPPER_PLATE_LORE2 = "item.copper-plate.lore.2";
+        public static String COPPER_PLATE = "item.copper_plate.name";
+        public static String COPPER_PLATE_LORE1 = "item.copper_plate.lore.1";
+        public static String COPPER_PLATE_LORE2 = "item.copper_plate.lore.2";
 
-        public static String COPPER_WIRE = "item.copper-wire.name";
-        public static String COPPER_WIRE_LORE1 = "item.copper-wire.lore.1";
-        public static String COPPER_WIRE_LORE2 = "item.copper-wire.lore.2";
+        public static String COPPER_WIRE = "item.copper_wire.name";
+        public static String COPPER_WIRE_LORE1 = "item.copper_wire.lore.1";
+        public static String COPPER_WIRE_LORE2 = "item.copper_wire.lore.2";
 
-        public static String COPPER_ORE = "item.copper-ore.name";
-        public static String COPPER_ORE_LORE1 = "item.copper-ore.lore.1";
-        public static String COPPER_ORE_LORE2 = "item.copper-ore.lore.2";
+        public static String COPPER_ORE = "item.copper_ore.name";
+        public static String COPPER_ORE_LORE1 = "item.copper_ore.lore.1";
+        public static String COPPER_ORE_LORE2 = "item.copper_ore.lore.2";
     }
 
     public static class Block {
@@ -54,6 +54,6 @@ public class I18nConst {
     }
 
     public static class Inventory {
-        public static String IW_CRAFTING_TABLE_TITLE = "inventory.iw-crafting-table.title";
+        public static String IW_CRAFTING_TABLE_TITLE = "inventory.iw_crafting_table.title";
     }
 }
