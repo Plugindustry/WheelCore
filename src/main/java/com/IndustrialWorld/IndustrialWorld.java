195ba91334a3fe8610cb69f06dc1e3784715c3d3
timetraveler314 <18847669238@163.com>
2020-02-03 15:27:19 +0800

    Add ConfigManager, config loading now moved to ConfigManager

--- src/main/java/com/IndustrialWorld/IndustrialWorld.java
+++ src/main/java/com/IndustrialWorld/IndustrialWorld.java
@@ -4,6 +4,7 @@ import com.IndustrialWorld.event.EventListener;
 import com.IndustrialWorld.event.TickEvent;
 import com.IndustrialWorld.i18n.I18n;
 import com.IndustrialWorld.manager.CommandManager;
+import com.IndustrialWorld.manager.ConfigManager;
 import com.IndustrialWorld.manager.MainManager;
 import com.IndustrialWorld.manager.RegisterManager;
 import com.IndustrialWorld.task.IWCraftingTableRegistrationTask;
@@ -18,9 +19,6 @@ import java.io.File;
 import java.io.IOException;
 
 public final class IndustrialWorld extends JavaPlugin {
-    public static YamlConfiguration blocksConfig;
-    public static YamlConfiguration config;
-
     public static IndustrialWorld instance;
 
     @Override
@@ -28,50 +26,27 @@ public final class IndustrialWorld extends JavaPlugin {
         // Plugin startup logic
         instance = this;
 
+        // Register command, EventListener and load config
         this.getCommand("iw").setExecutor(new CommandManager(this));
-
         getServer().getPluginManager().registerEvents(new EventListener(), this);
-        if (getDataFolder().isDirectory() || getDataFolder().mkdirs()) ;
-        File block_yml = new File(getDataFolder(), "blocks.yml");
-        if (!(block_yml.isFile())) {
-            try {
-                block_yml.createNewFile();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        blocksConfig = YamlConfiguration.loadConfiguration(block_yml);
-        MainManager.loadBlocksFromConfig(blocksConfig);
+        ConfigManager.init(this);
 
-        if (getDataFolder().isDirectory() || getDataFolder().mkdirs()) ;
-        File config_yml = new File(getDataFolder(), "config.yml");
-        if (!(config_yml.isFile())) {
-            saveDefaultConfig();
-        }
-        config = YamlConfiguration.loadConfiguration(config_yml);
-        I18n.init(config);
-
+        // Register recipes, blocks
         DebuggingLogger.debug("register recipes");
         RegisterManager.registerIWCRecipes();
         DebuggingLogger.debug("register blocks");
         RegisterManager.registerBlockIS();
-	    DebuggingLogger.debug("register crafting table.");
-        getServer().addRecipe(new ShapedRecipe(new NamespacedKey(this, "crafting_table_craft"), ConstItems.IW_CRAFTING_TABLE).shape("AAA", "ABA", "AAA").setIngredient('A', Material.IRON_INGOT).setIngredient('B', Material.CRAFTING_TABLE));
 
+        // Register TickEvent
         getServer().getScheduler().runTaskTimer(this, () -> getServer().getPluginManager().callEvent(new TickEvent()), 0, 0);
 
+        // Register crafting table recipe
         getServer().getScheduler().runTask(this, new IWCraftingTableRegistrationTask(this));
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        MainManager.saveBlocksToConfig(blocksConfig);
-        try {
-            blocksConfig.save(new File(getDataFolder(), "blocks.yml"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 
     public final String getDataFolderPath() {
