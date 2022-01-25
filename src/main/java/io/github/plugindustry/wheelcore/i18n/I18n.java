7d5b3e91571821fa81451f1b253338ca3c3bd0fb
czm23333 <34388355+czm23333@users.noreply.github.com>
2022-01-25 10:14:32 +0800

    Bug fix

--- src/main/java/io/github/plugindustry/wheelcore/i18n/I18n.java
+++ src/main/java/io/github/plugindustry/wheelcore/i18n/I18n.java
@@ -22,7 +22,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 
 import javax.annotation.Nonnull;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -51,7 +51,7 @@ public class I18n {
     /**
      * @param reader Lang reader to load
      */
-    public static void load(@Nonnull Locale locale, @Nonnull InputStreamReader reader) {
+    public static void load(@Nonnull Locale locale, @Nonnull Reader reader) {
         try {
             Map<String, String> kvMap;
             if (locales.containsKey(locale))
